b1d87cf4ea2ba89e1e8da2415a2258c67294b57e
Theo Wolf <theo@carbonre.com>
2024-07-01 21:24:58 +0100

    Fix dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,55 +1,79 @@
 """streamlit dashboard for the project"""
-import streamlit as st
-import plotly.express as px
-import numpy as np
-import pid_utils
 import time
 
-# Comibine with first running decorator
-@st.cache_data(persist=True)
-def run_on_startup():
-    return [], []
+import numpy as np
+import plotly.express as px
+import streamlit as st
 
+import pid_utils
 
-# Ensure the startup function is run
-states, targets = run_on_startup()
-
-st.title("Welcome to the Streamlit Dashboard for the project")
-st.write(
-    "This is a dashboard for the project. You can use this dashboard to interact with the project and see the results."
-)
-system_select = st.selectbox("System", ["System 1", "System 2"])
-# reset = st.button("Reset", on_click=reset_vals)
+st.title("Introduction to PID Control")
+st.write("Simple dashboard to demonstrate PID control")
 
 st.sidebar.title("Settings")
 kp = st.sidebar.slider(
     "Kp",
     0.0,
-    10.0,
+    1.0,
     0.4,
     0.01,
 )
-ki = st.sidebar.slider("Ki", 0.0, 10.0, 0.2, 0.01)
-kd = st.sidebar.slider("Kd", 0.0, 10.0, 0.01, 0.01)
-control_freq = st.sidebar.slider("Control frequency (s)", 0.1, 1.0, 0.1, 0.01)
+ki = st.sidebar.slider("Ki", 0.0, 1.0, 0.2, 0.01)
+kd = st.sidebar.slider("Kd", 0.0, 1.0, 0.01, 0.01)
+control_freq = st.sidebar.slider("Control frequency (s)", 0.01, 1.0, 0.1)
 system_target = st.sidebar.slider("System target", 0.0, 10.0, 2.0)
-noise = st.sidebar.slider("System noise", 0.0, 1.0, 0.01)
+noise = st.sidebar.slider("System noise", 0.0, 1.0, 0.1)
+init_state = st.sidebar.slider("Initial state", 0.0, 10.0, 5.0)
 
+# init vars
+integral = 0.0
+prev_error = 0.0
+time_axis = [0.0]
+states = [init_state]
+errors = [prev_error]
+
+# Create plot placeholder
 plot_placeholder = st.empty()
-init_state = 1.0
-# need to cache current states and targets somehow
+
 while True:
     # need to update state properly
-    state, controls = pid_utils.run_pid(
-        kp, ki, kd, noise, target=system_target, dt=control_freq, init_state=init_state
+    state, integral, prev_error = pid_utils.run_pid(
+        kp,
+        ki,
+        kd,
+        noise,
+        target=system_target,
+        dt=control_freq,
+        init_state=init_state,
+        integral=integral,
+        prev_error=prev_error,
     )
-    targets.append(system_target)
+    # update the data
+    time_axis.append(time_axis[-1] + control_freq)
     states.append(state)
-    fig = px.line(y=states, title="System state")
+    errors.append(prev_error)
+
+    # plot the data
+    fig = px.line(
+        x=time_axis,
+        y=states,
+        title="Error: {:.3f}".format(np.mean(np.array(errors) ** 2)),
+    )
+    fig.update_layout(yaxis_title="State", xaxis_title="Time")
+    fig.add_hline(y=system_target, line_dash="dot", line_color="blue")
     plot_placeholder.plotly_chart(fig)
+
+    # update the state
     init_state = state
+    states = states[-100:]
+    time_axis = time_axis[-100:]
+
     # Pause for a short duration
-    time.sleep(1)
+    time.sleep(0.05)
 
+    states = states[-100:]
+    time_axis = time_axis[-100:]
+    errors = errors[-100:]
 
-# fig.add_hline(y=system_target, line_dash="dot", line_color="blue")
+    # Pause for a short duration
+    time.sleep(0.001)
--- pid_utils.py
+++ pid_utils.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 class PID:
-    def __init__(self, kp, ki, kd):
+    def __init__(self, kp, ki, kd, I = 0.):
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.prev_error = 0
-        self.Is = [0]
+        self.prev_error = 0.
+        self.Is = [I]
 
     def update(self, error, dt):
         P = self.kp * error
@@ -16,7 +16,7 @@ class PID:
         return P + self.Is[-1] + D
 
 class System:
-    def __init__(self, x0, noise=0.1):
+    def __init__(self, x0: float, noise=0.1):
         self.x = x0
         self.noise = noise
 
@@ -24,10 +24,10 @@ class System:
         self.x = self.x * 0.9 + u + np.random.randn() * self.noise
         return self.x
 
-def run_pid(kp, ki, kd, noise, target=2., dt=0.1, init_state=1.):
-    pid = PID(kp, ki, kd)
+def run_pid(kp, ki, kd, noise, target=2., dt=0.1, init_state=1., integral=0, prev_error=0):
+    pid = PID(kp, ki, kd, I=integral)
     system = System(init_state, noise=noise)
     error = target - system.x
     u = pid.update(error, dt)
     system.update(u)
-    return system.x, u
+    return system.x, pid.Is[-1], pid.prev_error
